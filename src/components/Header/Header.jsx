9ebcfa96dc5494acbd96d82b1796ae753463da58
ubermensch <155133944+AlpianPPLG@users.noreply.github.com>
2024-06-06 10:13:17 +0800

    Update Header.jsx
    update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Header.css";
 
 const Header = ({ headTitle, headerExpanded }) => {
+  const [displayText, setDisplayText] = useState("");
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (index < headTitle.length) {
+        setDisplayText(displayText + headTitle.charAt(index));
+        setIndex(index + 1);
+      } else {
+        setDisplayText("");
+        setIndex(0);
+      }
+    }, 200);
+
+    return () => clearInterval(interval);
+  }, [headTitle, index]);
+
   return (
     <div className="head-container">
-      <h1 onClick={() => {window.location.reload();}} className="head-text">{headTitle}</h1>
+      <h1 className="head-text" onClick={() => window.location.reload()}>
+        {displayText}
+      </h1>
     </div>
   );
 };
+
 export default Header;
